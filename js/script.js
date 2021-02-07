c53eba570955b94423e1c6d0f27bee45d019b672
shivankar98 <47537297+shivankar98@users.noreply.github.com>
2021-02-07 11:11:56 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function shrink(){
+    document.querySelector(".popup").style.bottom="-200px";
+    document.querySelector(".popupHeading").style.borderRadius=("48px 48px 0 0");
+    document.querySelector(".cross").style.opacity="0";
+
+
+}
+function pop(){
+    document.querySelector(".popup").style.bottom="0";
+    document.querySelector(".popupHeading").style.borderRadius=("48px 0 0 0");
+    document.querySelector(".cross").style.opacity="1";
+}
+
+
+// navbar hover effects
+
+function homeHover(){
+    document.querySelector(".aboutUsNav").style.opacity="0.6";
+    document.querySelector(".authorsNav").style.opacity="0.6";
+    document.querySelector(".contactNav").style.opacity="0.6";
+}
+function aboutUsHover(){
+    document.querySelector(".homeNav").style.opacity="0.6";
+    document.querySelector(".authorsNav").style.opacity="0.6";
+    document.querySelector(".contactNav").style.opacity="0.6";
+}
+function authorsHover(){
+    document.querySelector(".aboutUsNav").style.opacity="0.6";
+    document.querySelector(".homeNav").style.opacity="0.6";
+    document.querySelector(".contactNav").style.opacity="0.6";
+}
+function contactHover(){
+    document.querySelector(".aboutUsNav").style.opacity="0.6";
+    document.querySelector(".authorsNav").style.opacity="0.6";
+    document.querySelector(".homeNav").style.opacity="0.6";
+}
+
+
+function hoverend(){
+    document.querySelector(".homeNav").style.opacity="1";
+    document.querySelector(".aboutUsNav").style.opacity="1";
+    document.querySelector(".authorsNav").style.opacity="1";
+    document.querySelector(".contactNav").style.opacity="1";
+}
+
+function unchk(){
+    document.getElementById("chk").checked = false;
+}
